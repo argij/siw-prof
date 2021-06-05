e3981310c14b90f071962ed42d51591360137354
argij <jackflame1600@gmail.com>
2021-06-05 19:23:01 +0200

    pulizie varie

--- src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
+++ src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
@@ -1,4 +1,4 @@
-package it.uniroma3.siw.authentication;
+ package it.uniroma3.siw.authentication;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
