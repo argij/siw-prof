d5b79df1d9e6c65e241ce10cd8ad27f96486dc13
argij <jackflame1600@gmail.com>
2021-06-11 16:17:41 +0200

    x

--- src/main/java/it/uniroma3/siw/spring/model/Credentials.java
+++ src/main/java/it/uniroma3/siw/spring/model/Credentials.java
@@ -5,10 +5,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 
 @Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class Credentials {
 	
 	public static final String DEFAULT_ROLE = "DEFAULT";
@@ -30,43 +37,4 @@ public class Credentials {
 	@OneToOne(cascade = CascadeType.ALL)
 	private User user;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
 }
